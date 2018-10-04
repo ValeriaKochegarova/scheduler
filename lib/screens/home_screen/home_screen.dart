5b52d89bb1c1a0f94dc2747c37e5ee028df8e0d0
unnamed92 <miketkachenko92@gmail.com>
2018-10-04 16:19:26 +0300

    initial commit

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:scheduler_app/screens/home_screen/delas.widget.dart';
+import 'package:scheduler_app/screens/home_screen/progressBar.widget.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -13,11 +14,40 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: Text('text'),
       ),
-      body: Container(
-        padding: EdgeInsets.all(32.0),
-        child: Center(
-          child: DealsWidget()
-        ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(14.0),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    const ListTile(
+                      title: Text(
+                        'SURVEY ANSWERS',
+                        style: TextStyle(
+                            color: Colors.lightBlueAccent,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      // chart
+                    ),
+                    Container(
+                        height: 180.0, child: DonutPieChart.withSampleData())
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(32.0),
+            child: Center(
+              child: DealsWidget()
+            ),
+          ),
+        ],
       ),
     );
   }
