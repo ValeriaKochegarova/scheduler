eb9b1f072419626e4d95d9b5064e0dbfe52056c7
Alexey Dolya <alexey@Alexeys-iMac.local>
2018-10-09 16:30:29 +0300

    add datepicker

--- lib/screens/create_deal/create_deal.screen.dart
+++ lib/screens/create_deal/create_deal.screen.dart
@@ -8,6 +8,8 @@ import 'package:scheduler_app/store/actions/deals.action.dart';
 import 'package:scheduler_app/store/reducers/reducer.dart';
 import 'package:scheduler_app/store/store.dart';
 
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+
 class CreateDealScreen extends StatefulWidget {
   @override
   _CreateDealScreenState createState() => _CreateDealScreenState();
@@ -16,8 +18,16 @@ class CreateDealScreen extends StatefulWidget {
 class _CreateDealScreenState extends State<CreateDealScreen> {
   final dateFormat = DateFormat("MM/d/yyyy 'at' h:mma");
 
+  final noteDateController = TextEditingController();
+
   num priority = 0;
   String text = '';
+
+  @override
+  void dispose() {
+    noteDateController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -56,6 +66,25 @@ class _CreateDealScreenState extends State<CreateDealScreen> {
           ColoredPallete(priority, (num priority) {
             this.priority = priority;
           }),
+          Text('Выберите дату:'),
+          Theme(
+            data: Theme.of(context).copyWith(
+                primaryColor: Colors.blue,
+                textSelectionColor: Colors.blue,
+                toggleableActiveColor: Colors.blue),
+            child: Container(
+              margin: EdgeInsets.all(5.0),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blueGrey[900])),
+              child: DateTimePickerFormField(
+                controller: noteDateController,
+                format: dateFormat,
+                onChanged: (noteDate) {
+                  print(noteDate);
+                },
+              ),
+            ),
+          ),
           IconButton(
             iconSize: 30.0,
             disabledColor: Colors.grey[400],
@@ -66,7 +95,7 @@ class _CreateDealScreenState extends State<CreateDealScreen> {
                     Map deal = {
                       'text': text,
                       'done': 0,
-                      'date': store.state.date,
+                      'date': noteDateController.text,
                       'priority': priority
                     };
                     state['createDeal'](deal);
